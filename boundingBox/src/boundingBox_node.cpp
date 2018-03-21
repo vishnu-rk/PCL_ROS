37507e4de8de15320e6e0b2571dd3105adfceabb
vishnu-rk <vishaxis@gmail.com>
2018-03-21 00:42:17 -0400

    2D bounding box through ROS topic for objects detected on tensorflow_object_detector

---
+++ boundingBox/src/boundingBox_node.cpp
@@ -0,0 +1,47 @@
+#include <ros/ros.h>
+#include <std_msgs/MultiArrayLayout.h>
+#include <std_msgs/MultiArrayDimension.h>
+#include <std_msgs/Float32MultiArray.h>
+#include "vision_msgs/Detection2DArray.h"
+
+
+ros::Publisher pub_display;
+
+void objects_cb (const vision_msgs::Detection2DArray& msg){
+// Create a container for the data.
+   std_msgs::Float32MultiArray output;
+   output.data.clear();
+   if (msg.detections.size()!=0){
+      if (msg.detections[0].results[0].id==47){
+      output.data.push_back(msg.detections[0].results[0].id);     
+      output.data.push_back(msg.detections[0].bbox.center.x);
+      output.data.push_back(msg.detections[0].bbox.center.y);
+      output.data.push_back(msg.detections[0].bbox.size_x);
+      output.data.push_back(msg.detections[0].bbox.size_y);
+      pub_display.publish (output);
+   }
+   }
+  
+   else
+   {
+      output.data.push_back(0.0);
+      pub_display.publish (output);   
+   }
+   
+ }
+ 
+ int main (int argc, char** argv)
+ {
+   // Initialize ROS
+   ros::init (argc, argv, "boundingBox_node");
+   ros::NodeHandle nh;
+ 
+   // Create a ROS subscriber for the input point cloud
+   ros::Subscriber sub = nh.subscribe ("/objects", 1, objects_cb);
+ 
+   // Create a ROS publisher for the output point cloud
+   pub_display = nh.advertise<std_msgs::Float32MultiArray> ("/objects/PoseArray", 1);
+ 
+   // Spin
+   ros::spin ();
+}
